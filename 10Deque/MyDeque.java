a125e2551f73c468776830fbfc0f12adbd322e11
Joshua Turcotti <turcotti.joshua@gmail.com>
2017-04-24 15:21:48 -0400

    removed debugging catches and moved tests

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -38,8 +38,7 @@ class GenDeque<E> {
 	for (int i=tail; i<=head; i++)
 	    fresh[Math.floorMod(i, body.length*2)] = body[mod(i)];
 
-	body = fresh;
-    }
+	body = fresh;}
 
     private void checkRemove() {
 	if (size()==0)
@@ -62,14 +61,8 @@ class GenDeque<E> {
 	return uncheck(body[mod(head--)]);}
     
     public void addLast(E e) {
-	try {
-	    checkAdd();
-	    body[mod(--tail)] = e;}
-	catch (Exception ex) {
-	    System.out.println(Arrays.toString(body) + " body.length: " + body.length + " size: " + size() + " head: " + head + " " + mod(head) + " tail: " + tail + " " + mod(tail));
-	    System.out.println(this);
-	}
-    }
+	checkAdd();
+	body[mod(--tail)] = e;}
 
     public E getLast() {
 	checkRemove();
@@ -86,31 +79,9 @@ class GenDeque<E> {
 	for (int j=tail; j<=head; j++)
 	    out[i++] = body[mod(j)];
 	
-	return (E[]) out;
-    }
+	return (E[]) out;}
 
     public String toString() {
-	return Arrays.toString(toArray());
-    }
-
-    public static void main(String[] args) {
-	MyDeque md = new MyDeque();
-	for(int i = 0; i<20; md.addFirst("apple")) {i++;}
-	System.out.println(" size: " + md.size());
-	for (int i=0; i<100; md.addLast(md.removeFirst())) {i++;}
-	System.out.println(" size: " + md.size());
-	while (true)
-	    try {
-		md.removeFirst();
-		md.removeLast();
-	    } catch (NoSuchElementException e) {
-		break;
-	    }
-	md.addFirst("banana");
-	for (int i=0; i<16; md.addFirst(md.getFirst() + "na")) {i++;}
-	System.out.println(" size: " + md.size());
-	for (int i=0; i<8; md.addLast(md.getLast() + "na")) {i++;}
-	System.out.println(" size: " + md.size());
-    }
+	return Arrays.toString(toArray());}
 }
     
--- 10Deque/Test.java
+++ 10Deque/Test.java
@@ -1,12 +1,23 @@
+import java.util.*;
+
 public class Test {
-    MyDeque md = new MyDeque();
-    for(int i = 0; i<20; md.addFirst(i++)) {}
-    for (int i=0; i<100; md.addLast(md.removeFirst())) {i++;}
-    while (true)
-	try {
-	    md.removeFirst();
-	    md.removeLast();
-	} catch (NoSuchElementException e) {
-	    break;
-	}
-	
+    public static void main(String[] args) {
+	MyDeque md = new MyDeque();
+	for(int i = 0; i<20; md.addFirst("apple")) {i++;}
+	System.out.println(" size: " + md.size());
+	for (int i=0; i<100; md.addLast(md.removeFirst())) {i++;}
+	System.out.println(" size: " + md.size());
+	while (true)
+	    try {
+		md.removeFirst();
+		md.removeLast();
+	    } catch (NoSuchElementException e) {
+		break;
+	    }
+	md.addFirst("banana");
+	for (int i=0; i<16; md.addFirst(md.getFirst() + "na")) {i++;}
+	System.out.println(" size: " + md.size());
+	for (int i=0; i<8; md.addLast(md.getLast() + "na")) {i++;}
+	System.out.println(" size: " + md.size());
+    }
+}
